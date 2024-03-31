92f62302547671ddc50908ec2c86811161198b2a
wellitsabhi <abhishekongit@gmail.com>
2024-03-31 11:04:53 +0530

    me added

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,20 @@
+import { connect } from '@/dbConfig/dbConfig';
+import User from '@/models/userModel';
+import { NextRequest, NextResponse } from 'next/server';
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { getDataFromToken } from '@/helpers/getdataFromToken';
+
+connect();
+
+export async function POST(request: NextRequest){
+    //extract data from token
+    const userId = await getDataFromToken(request)
+    const user = User.findOne({_id: userId}).select("-password")
+    // check if there is no user
+    return NextResponse.json({
+        message:"User found",
+        data: user
+    })
+
+}
