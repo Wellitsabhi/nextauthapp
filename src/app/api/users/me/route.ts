3f9bcb233eb36b1538884e2c65ed668dd95487b1
wellitsabhi <abhishekongit@gmail.com>
2024-03-31 16:47:45 +0530

    minor changes

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,20 +1,21 @@
 import { connect } from '@/dbConfig/dbConfig';
 import User from '@/models/userModel';
 import { NextRequest, NextResponse } from 'next/server';
-import bcryptjs from "bcryptjs";
-import jwt from "jsonwebtoken";
-import { getDataFromToken } from '@/helpers/getdataFromToken';
+import { getDataFromToken } from '@/helpers/getDataFromToken';
 
 connect();
 
-export async function POST(request: NextRequest){
-    //extract data from token
-    const userId = await getDataFromToken(request)
-    const user = User.findOne({_id: userId}).select("-password")
-    // check if there is no user
-    return NextResponse.json({
-        message:"User found",
-        data: user
-    })
-
+export async function GET(request: NextRequest) {
+    try {
+        //extract data from token
+        const userId = await getDataFromToken(request);
+        const user = await User.findOne({ _id: userId }).select("-password");
+        return NextResponse.json({
+            message: "User found",
+            data: user
+        })
+    } catch (error: any) {
+        // check if there is no user
+        return NextResponse.json({ error: error.message }, { status: 400 });
+    }
 }
